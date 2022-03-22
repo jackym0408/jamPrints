d5527ca648a044c180e34aa275a48f581018f54b
Jackeline Mendoza <jackelinemendoza@R2Hs-MBP.lan>
2022-03-22 16:18:46 -0400

    first commit

---
+++ src/components/nav.jsx
@@ -0,0 +1,23 @@
+import berryLogo from 
+
+function nav(){
+    return(
+        <>
+            <nav className="navbar"
+                    <img src={berryLogo} alt="strawberry logo"/>
+                    <ul>
+                        <li> <a href="index.html">Home</a> </li>
+                        <li> <a href="product.html">Products</a> </li>
+                        <li> <a href="contact.html">Contact Us</a> </li>
+                    </ul>
+                </nav>
+        </>
+    );
+}
+
+export default nav;
+
+
+
+
+
