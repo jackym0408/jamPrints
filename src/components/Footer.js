dc8b3c9e60f3d5881d9ec6a76075ccd4ea6e1122
Jackeline Mendoza <jackelinemendoza@R2Hs-MBP.lan>
2022-03-24 02:16:07 -0400

    footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,21 @@
 // insert footer stuff
+
+import React from 'react';
+
+function footer(){
+    return(
+        <>
+            <footer>
+                <a href="https://www.facebook.com/" class="fa fa-facebook" aria-label="facebook"></a>
+                <a href="https://twitter.com/" class="fa fa-twitter" aria-label="twitter"></a>
+                <a href="http://www.linkedin.com/" class="fa fa-linkedin" aria-label="linkedin"></a>
+                <a href="https://www.instagram.com/" class="fa fa-instagram" aria-label="instagram"></a> 
+                <p id="copyright"> &copy; Copyright 2021 </p> 
+                
+            </footer>
+        
+        </>
+    );
+}
+
+export default footer;
