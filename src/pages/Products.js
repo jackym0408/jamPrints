2f5a3595410df912ae7317260dbfe58aca57354b
Jackeline Mendoza <jackelinemendoza@R2Hs-MBP.lan>
2022-03-31 02:09:35 -0400

    added filter

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -15,13 +15,52 @@ export default class Products extends Component {
       //if heroku dies type 'heroku restart' in terminal
     });
   }
+  jamfilter(type){
+    return() => {
+      this.setState({type})
+    }
+  };
+
   render() {
     return (
       <>
+      <button className='filterbutton' onClick={this.jamfilter(100)}>
+        {'more than $100'}
+      </button>
+
+      <button className='filterbutton' onClick={this.jamfilter(80)}>
+        {'less than $80'}
+      </button>
+
+      <button className='filterbutton' onClick={this.jamfilter('cartoons')}>
+        {'cartoons'}
+      </button>
+
+      <button className='filterbutton' onClick={this.jamfilter('anime')}>
+        {'anime'}
+      </button>
+
+      <button className='filterbutton' onClick={this.jamfilter('original')}>
+        {'originals'}
+      </button>
+
+      
         <div className="productsContainer">
         {
           //.map
-          this.state.productData.map((card) => {
+          this.state.productData
+          .filter((card) => {
+            if (this.state.type === "Reset") {
+              return true;
+            } else if (this.state.type === 100) {
+              return this.state.type <= card.price;
+            } else if (Number.isInteger(this.state.type)) {
+              return this.state.type > card.price;
+            } else {
+              return this.state.type === card.genre;
+            }
+          })
+          .map((card) => {
             return(
               <div className="productImages">
                 <img src={card.image} alt={card.product_name} width="300" height="auto"/>
